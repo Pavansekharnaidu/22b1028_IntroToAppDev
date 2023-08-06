62b5503c752aab85b574df55e24d6ed53b2c94a8
pavansekharnaidu <pavansekharnaidu217@gmail.com>
2023-08-07 01:15:29 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,282 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:bittu/widget_tree.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyCikxd0689YBtbbx5K9C8x-bbyXhjBPRPg",
+        appId: "1:680376519859:android:c02e219b780bcf8b38b4c6",
+        messagingSenderId: '680376519859',
+        projectId: 'sahil-a4b2e'),
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Budget Tracker',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<Expense> expenses = [
-    Expense(description: 'Food', amount: 100.0, category: 'Food'),
-    Expense(description: 'Transportation', amount: 50.0, category: 'Transportation'),
-    Expense(description: 'Housing', amount: 300.0, category: 'Housing'),
-    Expense(description: 'Entertainment', amount: 200.0, category: 'Entertainment'),
-    Expense(description: 'Other', amount: 150.0, category: 'Other'),
-  ];
-
-  Future<void> _navigateToExpenseScreen() async {
-    final updatedExpenses = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => ExpenseScreen(expenses: expenses)),
-    );
-
-    if (updatedExpenses != null) {
-      setState(() {
-        expenses = updatedExpenses;
-      });
-    }
-  }
-
-  double calculateTotalExpense() {
-    return expenses.fold(0.0, (previous, current) => previous + current.amount);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double totalExpense = calculateTotalExpense();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Budget Tracker'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Pavan Sekhar Naidu',
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 12.0),
-            Text(
-              'Total Expenses',
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: ElevatedButton(
-        onPressed: _navigateToExpenseScreen,
-        child: Text('Show All Expenses'),
-      ),
-    );
-  }
-}
-
-class Expense {
-  String description;
-  double amount;
-  String category;
-
-  Expense({
-    required this.description,
-    required this.amount,
-    required this.category,
-  });
-}
-
-class ExpenseScreen extends StatefulWidget {
-  final List<Expense> expenses;
-
-  ExpenseScreen({required this.expenses});
-
-  @override
-  _ExpenseScreenState createState() => _ExpenseScreenState();
-}
-
-class _ExpenseScreenState extends State<ExpenseScreen> {
-  void _addExpense() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        String description = '';
-        double amount = 0.0;
-
-        return AlertDialog(
-          title: Text('Add Expense'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(hintText: 'Enter expense description'),
-                onChanged: (value) {
-                  description = value;
-                },
-              ),
-              SizedBox(height: 12),
-              TextField(
-                decoration: InputDecoration(hintText: 'Enter expense amount'),
-                onChanged: (value) {
-                  amount = double.tryParse(value) ?? 0.0;
-                },
-              ),
-            ],
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  widget.expenses.add(Expense(description: description, amount: amount, category: 'Other'));
-                  Navigator.pop(context, widget.expenses);
-                });
-              },
-              child: Text('Save'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _editExpense(int index) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        String description = widget.expenses[index].description;
-        double amount = widget.expenses[index].amount;
-
-        return AlertDialog(
-          title: Text('Edit Expense'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(hintText: 'Enter expense description'),
-                controller: TextEditingController(text: description),
-                onChanged: (value) {
-                  description = value;
-                },
-              ),
-              SizedBox(height: 12),
-              TextField(
-                decoration: InputDecoration(hintText: 'Enter expense amount'),
-                controller: TextEditingController(text: amount.toString()),
-                onChanged: (value) {
-                  amount = double.tryParse(value) ?? 0.0;
-                },
-              ),
-            ],
-          ),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  widget.expenses[index].description = description;
-                  widget.expenses[index].amount = amount;
-                  Navigator.pop(context, widget.expenses);
-                });
-              },
-              child: Text('Save'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _deleteExpense(int index) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Delete Expense'),
-          content: Text('Are you sure you want to delete this expense?'),
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  widget.expenses.removeAt(index);
-                  Navigator.pop(context, widget.expenses);
-                });
-              },
-              child: Text('Delete'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double totalExpense = widget.expenses.fold(0.0, (previous, current) => previous + current.amount);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Expense Screen'),
-      ),
-      body: Column(
-        children: [
-          Text('Total of all expenses: \$${totalExpense.toStringAsFixed(2)}', style: TextStyle(fontSize: 20.0)),
-          Expanded(
-            child: ListView.builder(
-              itemCount: widget.expenses.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(widget.expenses[index].description),
-                  subtitle: Text(widget.expenses[index].category),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('\$${widget.expenses[index].amount.toStringAsFixed(2)}'),
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                          _editExpense(index);
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          _deleteExpense(index);
-                        },
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _addExpense();
-        },
-        child: Icon(Icons.add),
-      ),
+      home: const WidgetTree(),
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ItemListWidget extends StatefulWidget {
+  @override
+  _ItemListWidgetState createState() => _ItemListWidgetState();
+}
+
+class _ItemListWidgetState extends State<ItemListWidget> {
+  List<String> expenses = [];
+  TextEditingController _expenseController = TextEditingController();
+  TextEditingController _moneyController = TextEditingController();
+  String selectedCategory = 'Food'; // Update the selected category.
+
+  List<String> categories = [
+    'Food',
+    'Transportation',
+    'Housing',
+    'Entertainment',
+    'Other',
+  ];
+
+  Map<String, int> items = {};
+  int total = 0;
+
+  double getTotalAmount() {
+    double totalAmount = 0.0;
+    for (String expense in expenses) {
+      final moneyPart = expense.split(' - ').last;
+      final money = double.parse(moneyPart);
+      totalAmount += money;
+    }
+    return totalAmount;
+  }
+
+  void addExpense(String expense, String category, double money) {
+    setState(() {
+      expenses.add('${expense} (${category}) - ${money.toStringAsFixed(2)}');
+    });
+  }
+
+  void removeExpense(String expense) {
+    setState(() {
+      expenses.remove(expense);
+    });
+  }
+
+  void fetchUserData() {
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid != null) {
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .collection('items')
+          .get()
+          .then((querySnapshot) {
+        setState(() {
+          items = {};
+          total = 0;
+          querySnapshot.docs.forEach((doc) {
+            String name = doc['name'];
+            int price = doc['price'];
+            items[name] = price;
+            total = total + price;
+          });
+        });
+      }).catchError((error) {
+        print("Error fetching data: $error");
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Budget Expense Tracker'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _expenseController,
+              decoration: InputDecoration(
+                labelText: 'Expense',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    _expenseController.clear();
+                  },
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+            ),
+            DropdownButton(
+              items: categories
+                  .map((e) => DropdownMenuItem(value: e, child: Text(e)))
+                  .toList(),
+              value: selectedCategory,
+              onChanged: (value) {
+                setState(() {
+                  selectedCategory = value as String; // Update the selected category if value is not null.
+                });
+              },
+            ),
+            TextField(
+              controller: _moneyController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Money',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    _moneyController.clear();
+                  },
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                String expense = _expenseController.text.trim();
+                double money = 0;
+
+                try {
+                  money = double.parse(_moneyController.text);
+                } catch (e) {
+                  // Handle parsing error gracefully (e.g., show a dialog).
+                  return;
+                }
+
+                if (expense.isNotEmpty && money > 0) {
+                  addExpense(expense, selectedCategory, money);
+                  _expenseController.clear();
+                  _moneyController.clear();
+                }
+              },
+              child: Text('Add Expense'),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: expenses.length,
+                itemBuilder: (context, index) {
+                  String expense = expenses[index];
+                  return Dismissible(
+                    key: Key(expense),
+                    onDismissed: (direction) {
+                      removeExpense(expense);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('$expense removed.'),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    },
+                    child: ListTile(
+                      title: Text(expense),
+                      trailing: IconButton(
+                        onPressed: () {
+                          removeExpense(expense);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('$expense removed.'),
+                              duration: Duration(seconds: 2),
+                            ),
+                          );
+                        },
+                        icon: Icon(Icons.delete),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Total Amount: \$${getTotalAmount().toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
